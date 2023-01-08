3dd6959bfd8e69d6b2ef235e7b821c9078b8ae72
drewdunne <57418947+drewdunne@users.noreply.github.com>
2023-01-07 20:11:24 -0500

    add ec2 deployment

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ variable vpc_cidr_block {}
 variable subnet_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+variable instance_type {}
+variable public_key_location {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -56,3 +59,81 @@ resource "aws_default_route_table" "main-rtb" {
         Name: "${var.env_prefix}-main-rtb"
     }
 }
+
+resource "aws_default_security_group" "default-sg" {
+    vpc_id = aws_vpc.myapp-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = [var.my_ip]
+    }
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        prefix_list_ids = []
+    }
+
+    tags = {
+        Name: "${var.env_prefix}-default-sg"
+    }
+}
+
+# Retrieve the correct AMI
+data "aws_ami" "latest-amazon-linux-image" {
+    most_recent = true
+    owners = ["137112412989"] #amazon
+    filter {
+        name = "name" #what are we filtering
+        values = ["amzn2-ami-kernel-*-x86_64-gp2"]
+    }
+    filter {
+        name = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+output "aws_ami_id" {
+    value = data.aws_ami.latest-amazon-linux-image.id
+}
+
+output "ec2_public_ip" {
+    value = aws_instance.myapp-server.public_ip
+}
+
+resource "aws_instance" "myapp-server" {
+    ami = data.aws_ami.latest-amazon-linux-image.id
+    instance_type = var.instance_type
+
+    subnet_id = aws_subnet.myapp-subnet-1.id
+    vpc_security_group_ids = [aws_default_security_group.default-sg.id]
+    availability_zone = var.avail_zone
+
+    associate_public_ip_address = true
+    key_name = aws_key_pair.ssh-key.key_name
+
+    user_data = file("/Users/andrewdunne/repos/terraform/myapp-server-user-data.sh")
+    user_data_replace_on_change = true
+
+    tags = {
+        Name = "${var.env_prefix}-server"
+    }
+}
+
+resource "aws_key_pair" "ssh-key" {
+    key_name = "server-key"
+    public_key = file(var.public_key_location)
+}
+
+
+
