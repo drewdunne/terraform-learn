3123e57e30935b34e7f6b23c24ca3d5cbd50d16a
drewdunne <57418947+drewdunne@users.noreply.github.com>
2023-01-07 23:54:19 -0500

    modularized subnet

--- main.tf
+++ main.tf
@@ -1,62 +1,7 @@
-provider "aws" {
-    region = "us-west-2"
-}
-
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
-variable my_ip {}
-variable instance_type {}
-variable public_key_location {}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
         Name: "${var.env_prefix}-vpc"
-    }
-}
-
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        Name: "${var.env_prefix}-subnet-1"
-    }
-}
-
-# resource "aws_route_table" "myapp-route-table" {
-#     vpc_id = aws_vpc.myapp-vpc.id
-#     route {
-#         cidr_block = "0.0.0.0/0"
-#         gateway_id = aws_internet_gateway.myapp-igw.id
-#     }
-#     tags = {
-#         Name: "${var.env_prefix}-rtb"
-#     }
-# }
-
-resource "aws_internet_gateway" "myapp-igw" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-        Name: "${var.env_prefix}-igw"
-    }
-}
-
-# resource "aws_route_table_association" "a-rtb-subnet" {
-#     subnet_id = aws_subnet.myapp-subnet-1.id
-#     route_table_id = aws_route_table.myapp-route-table.id
-# }
-
-resource "aws_default_route_table" "main-rtb" {
-    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-igw.id
-    }
-    tags = {
-        Name: "${var.env_prefix}-main-rtb"
     }
 }
 
@@ -89,6 +34,14 @@ resource "aws_default_security_group" "default-sg" {
     }
 }
 
+module "myapp-subnet" {
+    source = "./modules/subnet"
+    subnet_cidr_block = var.subnet_cidr_block
+    avail_zone = var.avail_zone
+    env_prefix = var.env_prefix
+    vpc_id = aws_vpc.myapp-vpc.id
+    default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+}
 # Retrieve the correct AMI
 data "aws_ami" "latest-amazon-linux-image" {
     most_recent = true
@@ -103,26 +56,18 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws_ami_id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
 resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
 
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet.subnet.id #'subnet' is the exported value in /modules/subnet/outputs.tf
     vpc_security_group_ids = [aws_default_security_group.default-sg.id]
     availability_zone = var.avail_zone
 
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("/Users/andrewdunne/repos/terraform/myapp-server-user-data.sh")
+    # user_data = file("/Users/andrewdunne/repos/terraform/myapp-server-user-data.sh")
     user_data_replace_on_change = true
 
     tags = {
